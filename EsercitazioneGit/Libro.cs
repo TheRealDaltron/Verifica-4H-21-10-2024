26e81168fb29c79c701b7c3b92bbd8e2e451b1d5
TheRealDaltron <92648564+TheRealDaltron@users.noreply.github.com>
2024-10-21 08:25:28 +0200

    Aggiunta proprietà

--- EsercitazioneGit/Libro.cs
+++ EsercitazioneGit/Libro.cs
@@ -8,5 +8,24 @@ namespace EsercitazioneGit
 {
     internal class Libro
     {
+        public string autore { get; set; }
+        public string titolo { get; set; }
+        public string editore { get; set; }
+        public int Anno { get { return anno; } set { if (anno < 0) { throw new Exception("Anno Invalido");} anno = value; } }
+        public int Pagine { get { return pagine; } set { if (pagine <= 0) { throw new Exception("Numero Pagine Invalido"); } pagine = value; } }
+
+        private int anno, pagine;
+
+        public Libro(string _autore, string _titolo, string _editore, int _anno, int _pagine, int _an, int _pag)
+        {
+            autore = _autore;
+            titolo = _titolo;
+            editore = _editore;
+            Anno = _an;
+            Pagine = _pag;
+        }
+
+
+
     }
 }
