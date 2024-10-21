26fbcd2be7e61a11250142091abc32e3e2953f79
TheRealDaltron <92648564+TheRealDaltron@users.noreply.github.com>
2024-10-21 08:31:05 +0200

    Aggiunta Parametri Biblioteca

---
+++ EsercitazioneGit/Biblioteca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercitazioneGit
+{
+    internal class Biblioteca
+    {
+        Libro libro;
+
+        private string nome, indirizzo, apertura, chiusura;
+
+        public Biblioteca(string _nome, string _indirizzo, string _apertura, string _chiusura)
+        {
+            this.nome = _nome;
+            this.indirizzo = _indirizzo;
+            this.apertura = _apertura;
+            this.chiusura = _chiusura;
+        }
+
+
+    }
+}
